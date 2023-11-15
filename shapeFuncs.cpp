2dcebed9d645cd66ff31918a6090ad4f91c6d477
Arthurwang8 <148507250+Arthurwang8@users.noreply.github.com>
2023-11-15 11:56:16 -0800

    Update shapeFuncs.cpp

--- shapeFuncs.cpp
+++ shapeFuncs.cpp
@@ -40,7 +40,9 @@ double dist = sqrt(distX+distY);
 
 
 void initPoint(struct Point *p, double xVal, double yVal) {
-  //return; //@@@ for a void function, the stub is just a bare return that does nothing
+
+(*p).x = xVal;
+(*p).y = yVal;
 }
 
 
