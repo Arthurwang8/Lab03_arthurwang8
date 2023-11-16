cd5bacdbe6657a710279f85e4e967d08ea375f7f
Arthurwang8 <148507250+Arthurwang8@users.noreply.github.com>
2023-11-15 19:24:00 -0800

    Update boxToStringTest.cpp

--- boxToStringTest.cpp
+++ boxToStringTest.cpp
@@ -9,10 +9,10 @@ int main() {
 
   struct Box b1, b2;
 
-  initPoint(&b1,3.0,4.0,2.0,1.0);  
+  initBox(&b1,3.0,4.0,2.0,1.0);  
   assertEquals("ul=(3,4),w=2,h=1", boxToString(b1), "boxToString(p1)");
 
-  initPoint(&b2,3.14159,6.2831853071,2.12321,1);
+  initBox(&b2,3.14159,6.2831853071,2.12321,1);
   assertEquals("ul=(3.14,6.28),w=2.12,h=1", boxToString(b2), "boxToString(b2)");
   assertEquals("ul=(3,6),w=2,h=1", boxToString(b2,1), "boxToString(b2,1)");
   assertEquals("ul=(3.141,6.283),w=2.123,h=1", boxToString(b2,4), "boxToString(b2,4)");
